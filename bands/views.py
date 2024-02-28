1ded88ebd6de52ac69d7fee4a2e6d6af39a3e031
Sam Ordonez <samsonordonez.ce@gmail.com>
2024-02-28 07:36:00 -0800

    'refactored musicians django view, under bands app'

--- bands/views.py
+++ bands/views.py
@@ -1,6 +1,6 @@
 # RiffMates/bands/views.py
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator
 from bands.models import Musician
 
 def musician(request):
@@ -10,10 +10,34 @@ def musician(request):
 
     return render(request, "musician.html", data)
 
-
+'''
 def musicians(request):
     data = {
         'musicians':Musician.objects.all().order_by('last_name'),
+    }
+
+    return render(request, "musicians.html", data)
+
+    ### REFACTORED to use Paginator
+'''
+
+def musicians(request):
+    all_musicians = Musician.objects.all().order_by('last_name')
+    paginator = Paginator(all_musicians, 2)
+
+    page_num = request.GET.get('page', 1)
+    page_num = int(page_num)
+
+    if page_num < 1:
+        page_num = 1
+    elif page_num > paginator.num_pages:
+        page_num = paginator.num_pages
+
+    page = paginator.page(page_num)
+
+    data = {
+        'musicians':page.object_list,
+        'page':page,
     }
 
     return render(request, "musicians.html", data)
